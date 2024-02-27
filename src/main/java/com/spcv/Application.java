7c958610849db46f786298589b75345124fe2ea8
Your Full Name <Your Email>
2024-02-27 20:30:33 +0000

    refactor: moving dir structure

---
+++ src/main/java/com/spcv/Application.java
@@ -0,0 +1,10 @@
+package com.spcv;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
