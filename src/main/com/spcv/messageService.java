e563d7f0bb377545125e0ae105601d570128df26
Your Full Name <Your Email>
2024-02-27 20:46:21 +0000

    refactor: moving files

--- src/main/com/spcv/messageService.java
+++
@@ -1,9 +0,0 @@
-package com.spcv;
-
-import io.micronaut.core.annotation.NonNull;
-import jakarta.validation.constraints.NotBlank;
-
-interface MessageService {
-
-    String sayHello(@NonNull @NotBlank String name);
-}
